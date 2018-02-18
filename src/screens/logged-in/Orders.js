9e9f7e84aad334e3dc75c1ff79408e26a25a9db5
Joe Gaudet <joe.gaudet@food.ee>
2018-02-17 17:04:32 -0800

    Update react native

--- src/screens/logged-in/Orders.js
+++ src/screens/logged-in/Orders.js
@@ -1,18 +1,17 @@
 // libraries
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
   Animated,
   Button,
-  Dimensions,
-  Image,
+  Dimensions, FlatList,
+  Image, SectionList,
   StatusBar,
   StyleSheet,
   Text, TouchableOpacity,
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import RefreshableToolbar from "../../components/RefreshableToolbar";
-import {masterFoxClient} from "../../util/Auth";
+import { masterFoxClient } from "../../util/Auth";
 import moment from "moment";
 import Colors from "../../constants/colors";
 
@@ -25,11 +24,31 @@ let styles = StyleSheet.create({
     borderColor: '#d6d7da',
   },
   title: {
-    fontSize: 19,
+    textShadowColor: 'rgba(70, 70, 70, 1)',
+    textShadowOffset: {width: 1, height: 1},
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    textShadowColor: 'rgba(70, 70, 70, 1)',
+    textShadowOffset: {width: 1, height: 1},
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 14,
     fontWeight: 'bold',
   },
   background: {
     color: 'red',
+  },
+  section: {
+    padding: 5,
+    backgroundColor: Colors.FOODEE_RED
+  },
+  sectionText: {
+    color: 'white',
+    fontWeight: 'bold'
   },
   item: {
     paddingLeft: 15,
@@ -59,10 +78,6 @@ let styles = StyleSheet.create({
 });
 
 
-const TOOLBAR_HEIGHT = 60;
-const COVER_HEIGHT = 150;
-const PULL_TO_REFRESH_HEIGHT = 100;
-
 let restaurantId = 8;
 
 class Orders extends Component {
@@ -80,39 +95,9 @@ class Orders extends Component {
     date: moment()
   };
 
-  yPosition = new Animated.Value(0);
-  onScroll = Animated
-    .event([{
-        nativeEvent: {
-          contentOffset: {
-            y: this.yPosition
-          }
-        }
-      }],
-      {useNativeDriver: true}
-    );
-
   componentDidMount() {
     this._fetchData();
-    this.yPosition.addListener(this._handleScroll)
   }
-
-  componentWillUnmount() {
-    this.yPosition.removeListener(this._handleScroll)
-  }
-
-  _handleScroll = ({value}) => {
-    if (value < -PULL_TO_REFRESH_HEIGHT) {
-      this.setState({shouldRefresh: true});
-    }
-  };
-
-  _handleRelease = () => {
-    if (this.state.shouldRefresh) {
-      this.setState({shouldRefresh: false});
-      this._fetchData();
-    }
-  };
 
   _fetchData = async () => {
     this.setState({isRefreshing: true});
@@ -121,104 +106,109 @@ class Orders extends Component {
     const restaurant = await client.restaurants.get(restaurantId);
     this.setState({restaurant, isRefreshing: false});
 
-    const orders = await client.restaurants.from(restaurantId).index.orders({
+    let orders = await client.restaurants.from(restaurantId).index.orders({
       filter: {
         deliverOn: this.state.date.format('YYYY-MM-DD')
       },
       include: 'order-items.menu-item.menu-group,order-items.menu-option-items.menu-option-group'
     });
 
+    orders = Object.values(orders.reduce((acc, order) => {
+      const tranche = acc[order.tranche] || {
+        id: order.id,
+        title: order.tranche,
+        data: []
+      };
+
+      tranche.data.push(order);
+
+      acc[order.tranche] = tranche;
+
+      return acc;
+    }, {}));
+
     this.setState({orders});
   };
 
   render() {
 
-    // Avatar scale interoplations
-    const interpolateAvatarScale = this
-      .yPosition
-      .interpolate({
-        inputRange: [0, TOOLBAR_HEIGHT],
-        outputRange: [1, 0.6],
-        extrapolate: 'clamp'
-      });
-
     return (
-      <View
-        style={{position: 'absolute', backgroundColor: 'white', top: 0, right: 0, bottom: 0, left: 0}}
-      >
+      <View style={{display: 'flex', height: '100%', backgroundColor: 'white'}}>
         <StatusBar
           barStyle='light-content'
         />
-        <RefreshableToolbar
-          toolbarHeight={TOOLBAR_HEIGHT}
-          coverHeight={COVER_HEIGHT}
-          pullToRefreshHeight={PULL_TO_REFRESH_HEIGHT}
-          scrollPosition={this.yPosition}
-          isRefreshing={this.state.isRefreshing}
+        <View style={{position: 'relative'}}>
+          <Image
+            source={{uri: this.state.restaurant.coverImageUrl}}
+            style={{width: '100%', height: 200}}
+          />
 
-          coverUrl={this.state.restaurant.coverImageUrl}
-          title={this.state.restaurant.name}
-          subtitle={this.state.restaurant.subtitle}
-        />
-
-        <Animated.ScrollView
-          scrollEventThrottle={1}
-          onScroll={this.onScroll}
-          style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, paddingTop: COVER_HEIGHT - 25, zIndex: 1}}
-          onResponderRelease={this._handleRelease}
-        >
-
-          <View style={{paddingBottom: 800}}>
-            <View style={{display: 'flex', flexDirection: 'row'}}>
-              <Animated.Image
-                source={{uri: this.state.restaurant.thumbnailImageUrl}}
-                style={{
-                  width: 90,
-                  height: 90,
-                  borderColor: 'white',
-                  borderWidth: 4,
-                  marginLeft: 10,
-                  borderRadius: 45,
-                  transform: [{scale: interpolateAvatarScale}]
-                }}
-              />
-
-              <View style={{flex: 1, marginTop: 30, marginLeft: 10, backgroundColor: 'transparent'}}>
-                <Text style={{fontSize: 20, color: 'black'}}>{this.state.restaurant.name}</Text>
-                <Text style={{color: 'black'}}>{this.state.restaurant.subtitle}</Text>
-              </View>
-
+          <View style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            display: 'flex',
+            alignContent: 'center',
+            justifyContent: 'center'
+          }}>
+            <Animated.Image
+              source={{uri: this.state.restaurant.thumbnailImageUrl}}
+              style={{
+                width: 90,
+                height: 90,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                marginTop: 20,
+                marginBottom: 20,
+                borderColor: 'white',
+                borderWidth: 4,
+                borderRadius: 45
+              }}
+            />
+            <View>
+              <Text style={styles.title} textShadowColor="black">
+                {this.state.restaurant.name}
+              </Text>
+              <Text style={styles.subtitle}>{this.state.restaurant.subtitle} </Text>
             </View>
-
-            {this.renderOrders()}
           </View>
-        </Animated.ScrollView>
+
+        </View>
+
+        {
+          this.state.orders.length > 0
+            ? <SectionList
+              style={{flex: 1}}
+              renderSectionHeader={this.renderSectionHeader}
+              renderItem={this.renderItem}
+              sections={this.state.orders}
+              keyExtractor={(item, _) => item.id}
+            />
+            : this.renderEmptyState()
+        }
 
         {this.renderFooter()}
       </View>
     )
   }
 
-  renderOrders() {
-    return this.state.orders.length > 0 ?
-      this.state.orders.map(_ => this.renderOrder(_)) : this.renderEmptyState();
-  }
-
-  renderOrder(order) {
+  renderItem = ({item}) => {
     return (
       <TouchableOpacity
-        key={order.identifier}
-        onPress={() => this.props.navigation.navigate('Order', {order: order})}
+        key={item.identifier}
+        onPress={() => this.props.navigation.navigate('Order', {order: item})}
       >
         <View
           style={styles.item}
         >
           <View style={{flex: 1}}>
             <Text>
-              {order.identifier}
+              {item.identifier}
             </Text>
             <Text>
-              {order.numberOfPeople} people @ {moment(order.pickupAt).format('HH:MM a')}
+              {item.numberOfPeople} people @ {moment(item.pickupAt).format('HH:MM a')}
             </Text>
           </View>
           <Icon
@@ -229,44 +219,71 @@ class Orders extends Component {
         </View>
       </TouchableOpacity>
     );
-  }
+  };
+
+  renderSectionHeader = ({section}) => {
+    return (
+      <View style={styles.section}>
+        <Text style={styles.sectionText}>
+          {section.title}
+        </Text>
+      </View>
+    )
+  };
 
   renderEmptyState() {
     return (
-      <Image
-        style={{width: width, height: height / 2, marginLeft: 'auto', marginRight: 'auto'}}
-        source={require('../../img/empty-pink.png')}
-      />
+      <View>
+        <Image
+          style={{
+            width: width - (width / 4),
+            height: 250,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginTop: 80
+          }}
+          resizeMode={Image.resizeMode.contain}
+          source={require('../../img/empty-pink.png')}
+        />
+      </View>
     );
   }
 
   renderFooter() {
     return (
       <View style={styles.footer}>
-        <Icon
-          name="angle-left"
-          color={Colors.FOODEE_PINK}
-          size={30}
+        <TouchableOpacity
+          style={{width: 60}}
           onPress={() => {
             this.setState({date: moment(this.state.date).subtract(1, 'days')});
             this._fetchData();
           }}
-        />
+        >
+          <Icon
+            name="angle-left"
+            color={Colors.FOODEE_PINK}
+            size={30}
+          />
+        </TouchableOpacity>
         <View style={{flex: 1, display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
           <Text style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}}>
             {this.state.date.format('MMMM Do YYYY')}
           </Text>
         </View>
 
-        <Icon
-          name="angle-right"
-          color={Colors.FOODEE_PINK}
-          size={30}
+        <TouchableOpacity
+          style={{width: 60, display: 'flex', alignItems: 'flex-end'}}
           onPress={() => {
             this.setState({date: moment(this.state.date).add(1, 'days')});
             this._fetchData();
           }}
-        />
+        >
+          <Icon
+            name="angle-right"
+            color={Colors.FOODEE_PINK}
+            size={30}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
